30554746eb90690969a7975a46d42cd3cd39de5d
TanPOTGS <paulroque13@gmail.com>
2019-02-04 14:38:22 -0600

    added new images and new collision logic and run feature

--- js/script.js
+++ js/script.js
@@ -1,111 +1,247 @@
-let can;
-let ctx;
-let canW = null;
-let canH = null;
-let charImg = new Image();
-let player;
-charImg.src = "images/paul2.png";
+let can, ctx, canW = null, canH = null, player, obj;
+let playerImg = new Image();
+let bigTreeNight = new Image();
+playerImg.src = "images/paul2.png";
+bigTreeNight.src = "images/big_tree_night.png"
 
 function initCanvas() {
-    can = document.getElementById('game_canvas');//stores the canvas element
-    ctx = document.getElementById('game_canvas').getContext('2d');//stores the context of the canvas element
-    canW = can.width;//stores the width of the canvas
-    canH = can.height;//stores the height of the canvas
-    window.addEventListener('keydown', controller.keyPressedOrReleased);//adds an event listener to the window, listens for 'keydown', calls controller.keyPressedOrReleased
-    window.addEventListener('keyup', controller.keyPressedOrReleased);//adds an event listener to the window, listens for 'keyup', calls controller.keyPressedOrReleased
+    can = document.getElementById('game_canvas'); //stores the canvas element
+    ctx = document.getElementById('game_canvas').getContext('2d'); //stores the context of the canvas element
+    canW = can.width; //stores the width of the canvas
+    canH = can.height; //stores the height of the canvas
 
+    window.addEventListener('keydown', controller.keyPressedOrReleased); //adds an event listener to the window, listens for 'keydown', calls controller.keyPressedOrReleased
+    window.addEventListener('keyup', controller.keyPressedOrReleased); //adds an event listener to the window, listens for 'keyup', calls controller.keyPressedOrReleased
+
+    createAssets();
+    requestAnimationFrame(updateFrame);
+}
+
+function createAssets() {
     player = new Player();
-    requestAnimationFrame(frameUpdate);
+    obj = new GameAsset();
 }
 
-function frameUpdate() {
-    player.update();
+function updateFrame() {
     controller.status();
-    requestAnimationFrame(frameUpdate);
+    player.update();
+    obj.update();
+    requestAnimationFrame(updateFrame); //updateFrame will be called every frame.
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------Object------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+class GameAsset {
+    constructor() {
+        this.xPos = 245;
+        this.yPos = 395;
+        this.bigTreeNightOriginX = 200;
+        this.bigTreeNightOriginY = 200;
+        this.width = 190;
+        this.height = 75;
+        this.spriteOriginX = 0;
+        this.spriteOriginY = 0;
+        this.spriteW = bigTreeNight.width;
+        this.spriteH = bigTreeNight.height;
+        this.isbeingTouched = false;
+    }
+
+    get topSide() {return this.yPos};
+    get rightSide() {return this.xPos + this.width};
+    get bottomSide() {return this.yPos + this.height};
+    get leftSide() {return this.xPos};
+    get centerX() {return this.xPos + (this.width / 2)};
+    get centerY() {return this.yPos + (this.height / 2)};
+
+    render() {
+        /*if (this.isbeingTouched) {
+            this.color = "blue";
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.xPos, this.yPos, this.width, this.height);
+        } else {
+            this.color = "rgba(0, 0, 0, 0.5)";
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.xPos, this.yPos, this.width, this.height);
+        }*/
+
+        ctx.drawImage(bigTreeNight, this.spriteOriginX, this.spriteOriginY, this.spriteW, this.spriteH, this.bigTreeNightOriginX, this.bigTreeNightOriginY, this.spriteW, this.spriteH);
+        
+    }
+
+    collisionDetection() {
+        (this.topSide > player.bottomSide || this.rightSide < player.leftSide || this.bottomSide < player.topSide || this.leftSide > player.rightSide) ? this.isbeingTouched = false : this.isbeingTouched = true;
+    }
+
+    update() {
+        this.render();
+        this.collisionDetection();
+    }
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------Player----------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 class Player {
     constructor() {
-        this.xPos = 895;
-        this.yPos = 175;
-        this.spriteSheetW = charImg.width;
-        this.spriteSheetH = charImg.height;
+        this.xPos = canW / 2; //player position on the canvas, also where the players starts the game
+        this.yPos = canH / 2; //player position on the canvas, also where the players starts the game
+        this.spriteSheetW = playerImg.width; //the width of my player's full sprite sheet
+        this.spriteSheetH = playerImg.height; //the height of my player's full sprite sheet
         this.spritesPerRow = 2;
         this.spritesPerCol = 8;
-        this.spritesInASet = 2;
-        this.spriteW = this.spriteSheetW / this.spritesPerRow; //17,85
-        this.spriteH = this.spriteSheetH / this.spritesPerCol; //26,130
+        this.spritesInASet = 2; // each row is a set, and a different animation. there are 2 images per animation
+        this.individualSpriteW = this.spriteSheetW / this.spritesPerRow; //85px
+        this.individualSpriteH = this.spriteSheetH / this.spritesPerCol; //130px
         this.rowIndex = 0; //0 to 7
         this.columnIndex = 0; // 0 to 1
-        this.spriteXOrigin = this.spriteW * this.columnIndex;
-        this.spriteYOrigin = this.spriteH * this.rowIndex;
-        this.isMoving = false;
-        this.isMovingDiagonally = false;
-        this.movementSpeed; //defined in controller.status
-        this.animationCounter = 0;
+        this.spriteOriginX = this.individualSpriteW * this.columnIndex; //the origin of each sprite in a row
+        this.spriteOriginY = this.individualSpriteH * this.rowIndex; //the origin of each sprite in a column
+        this.isMoving = false; //the player starts the game not moving
+        this.isMovingDiagonally = false; //the player starts the game not moving in a diagonal 
+        this.movementSpeed; //defined in controller.status, how quickly the player moves across the canvas
+        this.animationCounter = 0; //in the player.update method, determines how quickly the animation switches between each image in the sprite set
+        this.isColliding = false; //if the player is touching another object or not
+        this.topCollide = false;
+        this.rightCollide = false;
+        this.bottomCollide = false;
+        this.leftCollide = false;
     }
+
+    get topSide() {return this.yPos}; //the top most coordinate of the player
+    get rightSide() {return this.xPos + this.individualSpriteW}; //the right most coordinate of the player
+    get bottomSide() {return this.yPos + this.individualSpriteH}; //the bottom most coordinate of the player
+    get leftSide() {return this.xPos}; //the left most coordinate of the player
+    get centerX() {return this.xPos + (this.individualSpriteW / 2)};
+    get centerY() {return this.yPos + (this.individualSpriteH / 2)};
 
     render() {
         this.animate();
         ctx.save();
         ctx.clearRect(0, 0, canW, canH);
-        ctx.drawImage(charImg, this.spriteXOrigin, this.spriteYOrigin, this.spriteW, this.spriteH, this.xPos, this.yPos, this.spriteW, this.spriteH); //(image, startX, startY, clipW, clipH, canvasX, canvasY, imageStretchW, imageStretchH)
+        ctx.drawImage(playerImg, this.spriteOriginX, this.spriteOriginY, this.individualSpriteW, this.individualSpriteH, this.xPos, this.yPos, this.individualSpriteW, this.individualSpriteH); //(image, imgOriginX, imgOriginY, howMuchWidthToShow, howMuchHeightToShow, whereToDisplayOnCanvasX, whereToDisplayOnCanvasX, imageStretchW, imageStretchH)
         ctx.restore();
     }
 
     animate() {
         let spriteIndex = Math.floor(this.animationCounter) % this.spritesInASet;
 
-        if(controller.dDownIsActive) {
+        if (controller.dDownIsActive) {
             this.rowIndex = 0;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
-        } else if(controller.dUpIsActive) {
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        } 
+        if (controller.dUpIsActive) {
             this.rowIndex = 1;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
-        } else if(controller.dLeftIsActive) {
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        }
+        if (controller.dLeftIsActive) {
             this.rowIndex = 2;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
-        } else if(controller.dRightIsActive) {
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        }
+        if (controller.dRightIsActive) {
             this.rowIndex = 3;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
-        } else if(controller.dDownIsActive && controller.dLeftIsActive) {
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        }
+        if (controller.dDownIsActive && controller.dLeftIsActive) {
             this.rowIndex = 4;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
-        } else if(controller.dRightIsActive && controller.dDownIsActive) {
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        }
+        if (controller.dRightIsActive && controller.dDownIsActive) {
             this.rowIndex = 5;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
-        } else if(controller.dLeftIsActive && controller.dUpIsActive) {
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        }
+        if (controller.dLeftIsActive && controller.dUpIsActive) {
             this.rowIndex = 6;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
-        } else if(controller.dUpIsActive && controller.dRightIsActive) {
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        }
+        if (controller.dUpIsActive && controller.dRightIsActive) {
             this.rowIndex = 7;
             this.columnIndex = spriteIndex;
-            this.spriteXOrigin = this.spriteW * this.columnIndex;
-            this.spriteYOrigin = this.spriteH * this.rowIndex;
+            this.spriteOriginX = this.individualSpriteW * this.columnIndex;
+            this.spriteOriginY = this.individualSpriteH * this.rowIndex;
+        }
+    }
+
+    moveUp() {
+        this.yPos -= this.movementSpeed; 
+        this.isMoving = true;
+    }
+
+    moveRight() {
+        this.xPos += this.movementSpeed;
+        this.isMoving = true;
+    }
+
+    moveDown() {
+        this.yPos += this.movementSpeed;
+        this.isMoving = true;
+    }
+
+    moveLeft() {
+        this.xPos -= this.movementSpeed;
+        this.isMoving = true;
+    }
+
+    collisionDetection() {
+        this.topCollide = false;
+        this.rightCollide = false;
+        this.bottomCollide = false;
+        this.leftCollide = false;
+
+        (this.topSide > obj.bottomSide || this.rightSide < obj.leftSide || this.bottomSide < obj.topSide || this.leftSide > obj.rightSide) ? this.isColliding = false : this.isColliding = true;
+
+        if (this.isColliding) {
+            let dX = this.centerX - obj.centerX;
+            let dY = this.centerY - obj.centerY;
+
+            if (Math.abs(dX) > Math.abs(dY)) {
+                if (dX > 0) {
+                    this.xPos = obj.xPos + obj.width;
+                    this.leftCollide = true;
+                } else {
+                    this.xPos = obj.xPos - this.individualSpriteW;
+                    this.rightCollide = true;
+                }
+            } else {
+                if (dY > 0) {
+                    this.yPos = obj.yPos + obj.height;
+                    this.topCollide = true;
+                } else {
+                    this.yPos = obj.yPos - this.individualSpriteH;
+                    this.bottomCollide = true;
+                }
+            }
         }
     }
 
     update() {
         this.render();
-        if(this.isMoving = true) {
+        this.collisionDetection();
+        if(this.isMoving) {
             this.animationCounter += 0.1;
         }
     }
 }
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------Controller---------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 let controller = {
     dLeftIsActive: false,
@@ -116,40 +252,23 @@ let controller = {
     keyPressedOrReleased: function(event) {
         let keyStatus;
 
-        if (event.type == "keydown") {
-            keyStatus = true;
-        } else {
-            keyStatus = false;
-        }
-      
+        (event.type == "keydown") ? keyStatus = true : keyStatus = false;
+
         switch(event.keyCode) {
             case 37:
             controller.dLeftIsActive = keyStatus;
-            //console.log(player.spriteXOrigin + "||" + player.spriteYOrigin);
-            //console.log(player.rowIndex + "||" + player.columnIndex);
-            //console.log("dLeftIsActive: " + controller.dLeftIsActive);
             break;
             case 39:
             controller.dRightIsActive = keyStatus;
-            //console.log(player.spriteXOrigin + "||" + player.spriteYOrigin);
-            //console.log(player.rowIndex + "||" + player.columnIndex);
-            //console.log("dRightIsActive: " + controller.dRightIsActive);
             break;
             case 38:
             controller.dUpIsActive = keyStatus;
-            //console.log(player.spriteXOrigin + "||" + player.spriteYOrigin);
-            //console.log(player.rowIndex + "||" + player.columnIndex);
-            //console.log("dUpIsActive: " + controller.dUpIsActive);
             break;
             case 40:
             controller.dDownIsActive = keyStatus
-            //console.log(player.spriteXOrigin + "||" + player.spriteYOrigin);
-            //console.log(player.rowIndex + "||" + player.columnIndex);
-            //console.log("dDownIsActive: " + controller.dDownIsActive);
             break;
             case 70:
             controller.runIsActive = keyStatus
-            //console.log("runIsActive: " + controller.runIsActive);
             break;
         }
     },
@@ -157,26 +276,19 @@ let controller = {
         player.isMoving = false;
 
         if (controller.dLeftIsActive) {
-            player.xPos -= player.movementSpeed;
-            player.isMoving = true;
+            player.moveLeft();
         }
         if (controller.dRightIsActive) {
-            player.xPos += player.movementSpeed;
-            player.isMoving = true;
+            player.moveRight();
         }
         if (controller.dUpIsActive) {
-            player.yPos -= player.movementSpeed;
-            player.isMoving = true;
+            player.moveUp();
         }
         if (controller.dDownIsActive) {
-            player.yPos += player.movementSpeed;
-            player.isMoving = true;
+            player.moveDown();
         }
-        if (controller.runIsActive) {
-            player.movementSpeed = 4.5;
-        } else {
-            player.movementSpeed = 3;
-        }
+
+        (controller.runIsActive) ? (player.movementSpeed = 5, player.animationCounter += 0.15) : player.movementSpeed = 3;
     }
 };
 
