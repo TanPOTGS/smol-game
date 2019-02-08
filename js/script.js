523dbd8591d2cc7469635a12fceffbf6a572c95b
TanPOTGS <paulroque13@gmail.com>
2019-02-08 11:51:31 -0600

    added final collision code, added another canvas, splits trees images

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 /*Global vars and images*/
-let forecan, backcan, forectx, backctx, forecanW = null, backcanW = null, forecanH = null, backcanH = null, player, obj;
+let forecan, midcan, backcan, forectx, midctx, backctx, forecanW = null, midcanW = null, backcanW = null, forecanH = null, midcanH = null, backcanH = null, player, background;
 
 let backgroundImg = new Image();
 let playerImg = new Image();
-let bigTreeNight = new Image();
+let bigTreeNightImg = new Image();
+let bigTreeNightTopImg = new Image();
+let bigTreeNightBottomImg = new Image();
 backgroundImg.src = "images/background.png";
-playerImg.src = "images/paul2.png";
-bigTreeNight.src = "images/big_tree_night.png";
+playerImg.src = "images/paul.png";
+bigTreeNightImg.src = "images/big_tree_night.png";
+bigTreeNightTopImg.src = "images/big_tree_night_top.png";
+bigTreeNightBottomImg.src = "images/big_tree_night_bottom.png"
+
+let trees = [];
 
 function initCanvas() {
-    forecan = document.getElementById('foreground_canvas'); //stores the  top canvas element
+    forecan = document.getElementById('foreground_canvas'); //stores the top canvas element
     forectx = document.getElementById('foreground_canvas').getContext('2d'); //stores the context of the top canvas element
-    backcan = document.getElementById('background_canvas'); //stores the  back canvas element
+    midcan = document.getElementById('midground_canvas'); //stores the middle canvas element
+    midctx = document.getElementById('midground_canvas').getContext('2d'); //stores the context of the middle canvas element
+    backcan = document.getElementById('background_canvas'); //stores the back canvas element
     backctx = document.getElementById('background_canvas').getContext('2d'); //stores the context of the back canvas element
     
     forecanW = forecan.width; //stores the width of the top canvas
     forecanH = forecan.height; //stores the height of the top canvas
+    midcanW = midcan.width; //stores the width of the middle canvas
+    midcanH = midcan.height; //stores the height of the middle canvas
     backcanW = backcan.width; //stores the width of the back canvas
     backcanH = backcan.height; //stores the height of the back canvas
 
@@ -26,23 +36,29 @@ function initCanvas() {
     requestAnimationFrame(updateFrame);
 }
 
-//--------------------------------------------------------------------------------------------------------------------------------------
-//------------------------------------------------------------Background----------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------------
-
 function createAssets() {
     background = new Background();
     player = new Player();
-    obj = new GameAsset();
+    trees[0] = new Obj(100, 100);
+    trees[1] = new Obj(850, 100);
+    trees[2] = new Obj(350, 400);
+    trees[3] = new Obj(300, 100);
 }
 
 function updateFrame() {
+    background.update();
     controller.status();
     player.update();
-    obj.update();
-    background.update();
+    trees[0].update();
+    trees[1].update();
+    trees[2].update();
+    trees[3].update();
     requestAnimationFrame(updateFrame); //updateFrame will be called every frame.
 }
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------Background------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class Background {
     constructor() {
@@ -57,67 +73,98 @@ class Background {
     render() {
         backctx.drawImage(backgroundImg, this.imgOriginX, this.imgOriginY, this.width, this.height, this.xPos, this.yPos, this.width, this.height);
     }
-
     update() {
         this.render();
     }
 }
 
-//--------------------------------------------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------Object------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------Object--------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class GameAsset {
-    constructor() {
-        this.xPos = 245;
-        this.yPos = 395;
-        this.bigTreeNightOriginX = 200;
-        this.bigTreeNightOriginY = 200;
-        this.width = 190;
-        this.height = 75;
+class Obj {
+    constructor(mapX, mapY) {
+        this.bigTreeNightOriginX = mapX;
+        this.bigTreeNightOriginY = mapY;
+        this.spritesW = bigTreeNightTopImg.width;
+        this.topSpriteH = bigTreeNightTopImg.height;
+        this.bottomSpriteH = bigTreeNightBottomImg.height;
+        this.hitBoxXPos = mapX + 45;
+        this.hitBoxYPos = mapY + 185;
+        this.hitBoxW = 190;
+        this.hitBoxH = 85;
         this.spriteOriginX = 0;
         this.spriteOriginY = 0;
-        this.spriteW = bigTreeNight.width;
-        this.spriteH = bigTreeNight.height;
-        this.isbeingTouched = false;
+        this.isColliding = false;
     }
 
-    get topSide() {return this.yPos};
-    get rightSide() {return this.xPos + this.width};
-    get bottomSide() {return this.yPos + this.height};
-    get leftSide() {return this.xPos};
-    get centerX() {return this.xPos + (this.width / 2)};
-    get centerY() {return this.yPos + (this.height / 2)};
+    get topSide() {return this.hitBoxYPos};
+    get rightSide() {return this.hitBoxXPos + this.hitBoxW};
+    get bottomSide() {return this.hitBoxYPos + this.hitBoxH};
+    get leftSide() {return this.hitBoxXPos};
+    get centerX() {return this.hitBoxXPos + (this.hitBoxW / 2)};
+    get centerY() {return this.hitBoxYPos + (this.hitBoxH / 2)};
 
     render() {
-        forectx.drawImage(bigTreeNight, this.spriteOriginX, this.spriteOriginY, this.spriteW, this.spriteH, this.bigTreeNightOriginX, this.bigTreeNightOriginY, this.spriteW, this.spriteH);
+        forectx.drawImage(bigTreeNightTopImg, this.spriteOriginX, this.spriteOriginY, this.spritesW, this.topSpriteH, this.bigTreeNightOriginX, this.bigTreeNightOriginY, this.spritesW, this.topSpriteH);
+        midctx.drawImage(bigTreeNightBottomImg, this.spriteOriginX, this.spriteOriginY, this.spritesW, this.bottomSpriteH, this.bigTreeNightOriginX, this.bigTreeNightOriginY + this.topSpriteH, this.spritesW, this.bottomSpriteH);
+        //forectx.strokeStyle = "black";
+        //forectx.strokeRect(this.hitBoxXPos, this.hitBoxYPos, this.hitBoxW, this.hitBoxH);
+        this.collisionDetection();
     }
-
     collisionDetection() {
-        (this.topSide > player.bottomSide || this.rightSide < player.leftSide || this.bottomSide < player.topSide || this.leftSide > player.rightSide) ? this.isbeingTouched = false : this.isbeingTouched = true;
-    }
+        let disX = Math.abs(this.centerX - player.centerX);
+        let disY = Math.abs(this.centerY - player.centerY);
 
+        if (disX > ((this.hitBoxW / 2) + (player.hitBoxW / 2)) || disY > ((this.hitBoxH / 2) + (player.hitBoxH / 2))) {
+            this.isColliding = false;
+            player.isColliding = false;
+        } else {
+            this.isColliding = true;
+            player.isColliding = true;
+        }
+
+        if (this.isColliding) {
+            let dirX = player.centerX - this.centerX;
+            let dirY = player.centerY - this.centerY;
+
+            if (Math.abs(dirX / (this.hitBoxW / 2)) > Math.abs(dirY / (this.hitBoxH / 2))) {
+                if (dirX > 0) {
+                    player.hitBoxXPos = this.hitBoxXPos + this.hitBoxW;
+                } else {
+                    player.hitBoxXPos = this.hitBoxXPos - player.individualSpriteW;
+                }
+            } else {
+                if (dirY > 0) {
+                    player.hitBoxYPos = this.hitBoxYPos + this.hitBoxH;
+                } else {
+                    player.hitBoxYPos = this.hitBoxYPos - player.hitBoxH;
+                }
+            }
+        }
+    }
     update() {
         this.render();
-        this.collisionDetection();
     }
 }
 
-//--------------------------------------------------------------------------------------------------------------------------------------
-//----------------------------------------------------------------Player----------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------Player------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class Player {
     constructor() {
-        this.xPos = forecanW / 2; //player position on the top canvas, also where the players starts the game
-        this.yPos = forecanH / 2; //player position on the top canvas, also where the players starts the game
+        this.hitBoxXPos = forecanW / 2; //player hitbox is smaller than sprite, so a different xPos is needed
+        this.hitBoxYPos = forecanH / 2; //player hitbox is smaller than sprite, so a different yPos is needed
         this.spriteSheetW = playerImg.width; //the width of my player's full sprite sheet
         this.spriteSheetH = playerImg.height; //the height of my player's full sprite sheet
-        this.spritesPerRow = 2;
-        this.spritesPerCol = 8;
+        this.spritesPerRow = 2; //sprites per row on sprite sheet
+        this.spritesPerCol = 8; //sprites per column on sprite sheet
+        this.individualSpriteW = this.spriteSheetW / this.spritesPerRow; //player width: 85px
+        this.individualSpriteH = this.spriteSheetH / this.spritesPerCol; //player height: 130px
+        this.hitBoxW = this.individualSpriteW; //hitbox width
+        this.hitBoxH = this.individualSpriteH - 75; //hitbox height
         this.spritesInASet = 2; // each row is a set, and a different animation. there are 2 images per animation
-        this.individualSpriteW = this.spriteSheetW / this.spritesPerRow; //85px
-        this.individualSpriteH = this.spriteSheetH / this.spritesPerCol; //130px
         this.rowIndex = 0; //0 to 7
         this.columnIndex = 0; // 0 to 1
         this.spriteOriginX = this.individualSpriteW * this.columnIndex; //the origin of each sprite in a row
@@ -127,27 +174,26 @@ class Player {
         this.movementSpeed; //defined in controller.status, how quickly the player moves across the top canvas
         this.animationCounter = 0; //in the player.update method, determines how quickly the animation switches between each image in the sprite set
         this.isColliding = false; //if the player is touching another object or not
-        this.topCollide = false;
-        this.rightCollide = false;
-        this.bottomCollide = false;
-        this.leftCollide = false;
     }
 
-    get topSide() {return this.yPos}; //the top most coordinate of the player
-    get rightSide() {return this.xPos + this.individualSpriteW}; //the right most coordinate of the player
-    get bottomSide() {return this.yPos + this.individualSpriteH}; //the bottom most coordinate of the player
-    get leftSide() {return this.xPos}; //the left most coordinate of the player
-    get centerX() {return this.xPos + (this.individualSpriteW / 2)};
-    get centerY() {return this.yPos + (this.individualSpriteH / 2)};
+    get topSide() {return this.hitBoxYPos}; //the top most coordinate of the player
+    get rightSide() {return this.hitBoxXPos + this.hitBoxW}; //the right most coordinate of the player
+    get bottomSide() {return this.hitBoxYPos + this.hitBoxH}; //the bottom most coordinate of the player
+    get leftSide() {return this.hitBoxXPos}; //the left most coordinate of the player
+    get centerX() {return this.hitBoxXPos + (this.hitBoxW / 2)};
+    get centerY() {return this.hitBoxYPos + (this.hitBoxH / 2)};
+    get paulXPos() {return this.hitBoxXPos}; //where the img is drawn relative to the hitbox
+    get paulYPos() {return this.hitBoxYPos - 80}; //where the img is drawn relative to the hitbox
 
     render() {
         this.animate();
         forectx.save();
         forectx.clearRect(0, 0, forecanW, forecanH);
-        forectx.drawImage(playerImg, this.spriteOriginX, this.spriteOriginY, this.individualSpriteW, this.individualSpriteH, this.xPos, this.yPos, this.individualSpriteW, this.individualSpriteH); //(image, imgOriginX, imgOriginY, howMuchWidthToShow, howMuchHeightToShow, whereToDisplayOnTopCanvasX, whereToDisplayOnTopCanvasX, imageStretchW, imageStretchH)
+        forectx.drawImage(playerImg, this.spriteOriginX, this.spriteOriginY, this.individualSpriteW, this.individualSpriteH, this.paulXPos, this.paulYPos, this.individualSpriteW, this.individualSpriteH); //(image, imgOriginX, imgOriginY, howMuchWidthToShow, howMuchHeightToShow, whereToDisplayOnTopCanvasX, whereToDisplayOnTopCanvasY, imageStretchW, imageStretchH)
+        //forectx.strokeStyle = "black";
+        //forectx.strokeRect(this.hitBoxXPos, this.hitBoxYPos, this.hitBoxW, this.hitBoxH);
         forectx.restore();
     }
-
     animate() {
         let spriteIndex = Math.floor(this.animationCounter) % this.spritesInASet;
 
@@ -200,71 +246,33 @@ class Player {
             this.spriteOriginY = this.individualSpriteH * this.rowIndex;
         }
     }
-
     moveUp() {
-        this.yPos -= this.movementSpeed; 
+        this.hitBoxYPos -= this.movementSpeed
         this.isMoving = true;
     }
-
     moveRight() {
-        this.xPos += this.movementSpeed;
+        this.hitBoxXPos += this.movementSpeed;
         this.isMoving = true;
     }
-
     moveDown() {
-        this.yPos += this.movementSpeed;
+        this.hitBoxYPos += this.movementSpeed;
         this.isMoving = true;
     }
-
     moveLeft() {
-        this.xPos -= this.movementSpeed;
+        this.hitBoxXPos -= this.movementSpeed;
         this.isMoving = true;
     }
-
-    collisionDetection() {
-        this.topCollide = false;
-        this.rightCollide = false;
-        this.bottomCollide = false;
-        this.leftCollide = false;
-
-        (this.topSide > obj.bottomSide || this.rightSide < obj.leftSide || this.bottomSide < obj.topSide || this.leftSide > obj.rightSide) ? this.isColliding = false : this.isColliding = true;
-
-        if (this.isColliding) {
-            let dX = this.centerX - obj.centerX;
-            let dY = this.centerY - obj.centerY;
-
-            if (Math.abs(dX) > Math.abs(dY)) {
-                if (dX > 0) {
-                    this.xPos = obj.xPos + obj.width;
-                    this.leftCollide = true;
-                } else {
-                    this.xPos = obj.xPos - this.individualSpriteW;
-                    this.rightCollide = true;
-                }
-            } else {
-                if (dY > 0) {
-                    this.yPos = obj.yPos + obj.height;
-                    this.topCollide = true;
-                } else {
-                    this.yPos = obj.yPos - this.individualSpriteH;
-                    this.bottomCollide = true;
-                }
-            }
-        }
-    }
-
     update() {
         this.render();
-        this.collisionDetection();
         if(this.isMoving) {
             this.animationCounter += 0.1;
         }
     }
 }
 
-//--------------------------------------------------------------------------------------------------------------------------------------
-//-------------------------------------------------------------Controller---------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------Controller-----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 let controller = {
     dLeftIsActive: false,
